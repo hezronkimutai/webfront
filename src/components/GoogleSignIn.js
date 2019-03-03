9eb84ff3b8a002d85888508c427d5ac5adae7188
hezronkimutai <hezronchelimo.hc@gmail.com>
2019-03-03 21:39:46 +0300

    initial commit

---
+++ src/components/GoogleSignIn.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+// // import ReactDOM from 'react-dom';
+// import GoogleLogin from 'react-google-login';
+//
+// class GoogleSignIn extends Component {
+//   const responseGoogle = (response) => {
+//     console.log(response);
+//   }
+//
+//   render() {
+//     <GoogleLogin
+//       clientId="685008069228-21mbrsha3kta8iri3vgdsriuvihpqujr.apps.googleusercontent.com"
+//       buttonText="Login"
+//       onSuccess={responseGoogle}
+//       onFailure={responseGoogle}
+//     />,
+//     document.getElementById('googleButton')
+//   }
+// }
+//
+// // import { GoogleLogout } from 'react-google-login';
+// //    <GoogleLogout
+// //      buttonText="Logout"
+// //      onLogoutSuccess={logout}
+// //    >
+// //    </GoogleLogout>
